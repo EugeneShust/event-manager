fac3e1c7a18dee5f9f087091b5dec41f6d1775c1
jochen <jochen@byte23.net>
2024-10-29 16:38:43 +0100

    changed width, added borders

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { Navbar } from './index';
 export function Header() {
     return (
         <>
-            <div className="border-b-2 border-[#CACED5] mb-4">
+            <div className="border-b-2 border-r-2 border-l-2 border-[#CACED5] mb-4">
                 <Navbar />
             </div>
         </>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,9 +5,11 @@ export function Navbar() {
         <nav>
             <div className="navbar bg-[#374151]">
                 <div className="flex-1">
-                    <a className="btn btn-ghost border-2 border-[#CACED5] text-xl">
-                        /Event Manager/
-                    </a>
+                    <li className="btn btn-ghost border-b-2 border-[#CACED5] text-xl">
+                        <Link to="/" className="text-xl">
+                            /Event Manager/
+                        </Link>
+                    </li>
                 </div>
                 <div className="flex-none gap-2">
                     <div className="dropdown dropdown-end">
@@ -26,17 +28,8 @@ export function Navbar() {
                         </div>
                         <ul
                             tabIndex={0}
-                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-4 mr-4 w-16 p-2 shadow"
                         >
-                            <li>
-                                <a className="justify-between">
-                                    Profile
-                                    <span className="badge">New</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a>Settings</a>
-                            </li>
                             <li>
                                 <a>Logout</a>
                             </li>
