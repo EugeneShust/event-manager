0edd3bb8c5bdbc0a69b86b38e1d5f7b8ead6dc2f
jochen <jochen@byte23.net>
2024-10-29 15:20:58 +0100

    Added Navbar and picures

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,9 @@
-import { Navbar } from './';
+import { Navbar } from './Navbar';
 
 export const Header = () => {
     return (
         <header className="flex flex-row align-center justify-between m-4 p-2 bg-gray-400">
-            <img
-                className="h-10 w-20"
-                src="https://svgsilh.com/svg_v2/402758.svg"
-            />
+            <img className="h-10 w-20" src="../pictures/event-manager.jpg" />
             <div
                 id="search"
                 className="flex flex-row flex-nowrap gap-4 items-center border-3 rounded-md"
