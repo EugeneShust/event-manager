6ddfcec84c93fb1fac29c138de7d963d66128d56
Yevhen <eugene.shust@gmail.com>
2024-10-30 20:58:40 +0100

    Used authService instead of fetch

--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,44 +1,53 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { SignUpForm } from '../components';
+import { registerUser } from '../services';
 
 export const SignUpPage = () => {
     const [error, setError] = useState('');
     const navigate = useNavigate();
 
-    const handleSubmit = async (formData) => {
+    const handleSubmit = async (data) => {
         setError('');
         try {
-            const response = await fetch('http://localhost:3001/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formData),
-            });
+            const signInfo = await registerUser(data);
 
-            const data = await response.json();
+            console.log(signInfo);
 
-            if (response.ok) {
-                localStorage.setItem('userId', data.userId);
-                localStorage.setItem('userName', data.name);
-                localStorage.setItem('userEmail', data.email);
+            if (signInfo.error) throw new Error(signInfo.error);
 
-                if (data.role) {
-                    localStorage.setItem('userRole', data.role);
-                }
+            //navigate('/login');
 
-                navigate('/create-event');
-            } else {
-                if (data.error === 'USER_EXISTS') {
-                    setError('Account already exists. Please login instead');
-                    setTimeout(() => {
-                        navigate('/login');
-                    }, 2000);
-                } else {
-                    setError(data.message || 'An error occurred during signup');
-                }
-            }
+            // const response = await fetch('http://localhost:3001/api/users', {
+            //     method: 'POST',
+            //     headers: {
+            //         'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(formData),
+            // });
+
+            // const data = await response.json();
+
+            // if (response.ok) {
+            //     localStorage.setItem('userId', data.userId);
+            //     localStorage.setItem('userName', data.name);
+            //     localStorage.setItem('userEmail', data.email);
+
+            //     if (data.role) {
+            //         localStorage.setItem('userRole', data.role);
+            //     }
+
+            //     navigate('/create-event');
+            // } else {
+            //     if (data.error === 'USER_EXISTS') {
+            //         setError('Account already exists. Please login instead');
+            //         setTimeout(() => {
+            //             navigate('/login');
+            //         }, 2000);
+            //     } else {
+            //         setError(data.message || 'An error occurred during signup');
+            //     }
+            // }
         } catch (err) {
             setError('Connection error. Please try again.');
         }
