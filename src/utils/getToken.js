7f97b6c66c82ad8c03422b3c4c607616a51d9011
Yevhen <eugene.shust@gmail.com>
2024-10-31 18:25:37 +0100

    Fixed saving token, updated AuthContext state.

--- src/utils/getToken.js
+++ src/utils/getToken.js
@@ -1,4 +1,8 @@
 // Utility Module: Helper function to get token from LocalStorage.
 export const getToken = () => {
-    return localStorage.getItem('accessToken');
+    let token = localStorage.getItem('accessToken');
+    if (token && token.startsWith('"') && token.endsWith('"')) {
+        token = token.slice(1, -1);
+    }
+    return token;
 };
