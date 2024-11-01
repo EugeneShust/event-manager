1a4579f3c57306efd5a18704c415db3dc18a9bb0
jochen <jochen@byte23.net>
2024-11-01 11:37:44 +0100

    refactor eventService.js

--- src/services/eventService.js
+++ src/services/eventService.js
@@ -1,109 +1,58 @@
 // Service Layer: EventService manages API requests for events.
 
 import { date } from '../utils';
+import { fetchWithAuth, fetchWithoutAuth } from '../api/index';
 
-// Get events from API
-
-const allEventsUrl = 'https://events-api-on1o.onrender.com/api/events';
+// Get all events
 
 async function getAllEvents() {
-    try {
-        const response = fetch(allEventsUrl);
-        if (!response) throw new Error('Could not fetch all events');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+    return await fetchWithoutAuth('/events', {
+        method: 'GET',
+        body: JSON.stringify({}),
+    });
 }
 
 // Get event by id
 
 const eventId = integer;
-const singleEventUrl = `https://events-api-on1o.onrender.com/api/events/api/events/${eventId}`;
 
 async function getSingleEvent() {
-    try {
-        const response = fetch(singleEventUrl);
-        if (!response) throw new Error('Could not fetch the requested event');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+    return await fetchWithoutAuth(`/events/${eventId}`, {
+        method: 'GET',
+        body: JSON.stringify({}),
+    });
 }
 
 // Get upcoming events
 
-const futureEventsUrl =
-    'https://events-api-on1o.onrender.com/api/events/upcoming';
-
 async function getFutureEvents() {
-    try {
-        const response = fetch(futureEventsUrl);
-        if (!response) throw new Error('Could not fetch upcoming events');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+    return await fetchWithoutAuth('/upcoming', {
+        method: 'GET',
+        body: JSON.stringify({}),
+    });
 }
 
 // Create new event
 
-async function createNewEvent() {
-    try {
-        const response = await fetch(allEventsUrl, {
-            method: 'POST',
-            body: JSON.stringify({
-                title: '',
-                description: '',
-                date: date(),
-                location: '',
-            }),
-        });
-        if (!response) throw new Error('The event could not be created');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+async function createNewEvent(eventData) {
+    return await fetchWithAuth('/events', {
+        method: 'POST',
+        body: JSON.stringify(eventData),
+    });
 }
-
 // Update existing event
 
 async function updateEvent() {
-    try {
-        const response = fetch(singleEventUrl, {
-            method: 'PUT',
-            body: JSON.stringify({
-                title: '',
-                description: '',
-                date: date(),
-                location: '',
-            }),
-        });
-        if (!response) throw new Error('The event could not be updated');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+    return await fetchWithAuth(`/events/${eventId}`, {
+        method: 'PUT',
+        body: JSON.stringify(eventData),
+    });
 }
 
 // Delete event
 
 async function deleteEvent() {
-    try {
-        const response = fetch(singleEventUrl, {
-            method: 'DELETE',
-        });
-        if (!response) throw new Error('The event could not be deleted');
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
+    return await fetchWithAuth(`/events/${eventId}`, { method: 'DELETE' });
 }
 
 export {
