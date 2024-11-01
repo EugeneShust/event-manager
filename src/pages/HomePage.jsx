7ee28a5b66c50e7bae191e73bf0a9381eb93ec96
jochen <jochen@byte23.net>
2024-11-01 14:50:40 +0100

    HomePage fetch (not ready yet)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { Button } from '../components/common/Button';
+import { fetchWithoutAuth } from '../api';
 
 export function HomePage() {
+    useEffect(() => {
+        const getAllEvents = fetchWithoutAuth('/events', {
+            method: 'GET',
+        });
+    }, []);
+    // ...
+
     return (
         <>
             <div className="flex flex-row align-baseline justify-end">
